61ee38f98be9498c889dd016ac2db9fa40515de7
suzawa0420 <suzawa@photomo.jp>
2022-07-08 18:46:23 +0900

    フォローとアンフォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -69,12 +69,18 @@ class User < ApplicationRecord
   end
 
   def follow!(user)
-    following_relationships.create!(following_id: user.id)
+    user_id = get_user_id(user)
+    following_relationships.create!(following_id: user_id)
   end
 
   def unfollow!(user)
-    relation = following_relationships.find_by!(following_id: user.id)
+    user_id = get_user_id(user)
+    relation = following_relationships.find_by!(following_id: user_id)
     relation.destroy!
+  end
+
+  def has_followed?(user)
+    following_relationships.exists?(following_id: user.id)
   end
 
   def prepare_profile
@@ -86,6 +92,18 @@ class User < ApplicationRecord
       profile.avatar
     else
       'default-avatar.png'
+    end
+  end
+
+  private
+  def get_user_id(user)
+    # userがUserのインスタンスとparams[:account_id]の数字のみのパターンがある
+    if user.is_a?(User)
+      # user_id = user.id
+      user.id
+    else
+      # user_id = user
+      user
     end
   end
 
