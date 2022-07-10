ede377edea25afd573750d94c3c71641eff19c55
suzawa0420 <suzawa@photomo.jp>
2022-07-10 17:13:32 +0900

    ajaxによるいいね機能の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,7 +26,6 @@ import { csrfToken } from "rails-ujs"
 axios.defaults.headers.common["X-CSRF-Token"] = csrfToken()
 
 
-
 const handleHeartDisplay = (hasLiked) => {
   if (hasLiked){
     $(".active-heart").removeClass("hidden")
@@ -47,7 +46,11 @@ document.addEventListener("turbolinks:load", () => {
     $(".inactive-heart").on("click", () => {
       axios.post(`/articles/${articleId}/like`)
         .then((response) => {
-          console.log(response)
+          if (response.data.status == "ok"){
+            $(".active-heart").removeClass("hidden")
+            $(".inactive-heart").addClass("hidden")
+          }
+
         })
         .catch((e) => {
           window.alert("Error")
@@ -58,7 +61,10 @@ document.addEventListener("turbolinks:load", () => {
     $(".active-heart").on("click", () => {
       axios.delete(`/articles/${articleId}/like`)
         .then((response) => {
-          console.log(response)
+          if (response.data.status == "ok"){
+            $(".active-heart").addClass("hidden")
+            $(".inactive-heart").removeClass("hidden")
+          }
         })
         .catch((e) => {
           window.alert("Error")
