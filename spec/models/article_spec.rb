5ca89f51ca859a059d16c1a0881f6f126cc0444e
suzawa0420 <suzawa@photomo.jp>
2022-07-12 23:44:38 +0900

    rspecでcontextの利用

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
+  context "タイトルと内容が入力されている場合" do
+    before do
+      user = User.create!({
+        email: "test@example.com",
+        password: "passord"
+      })
+      @article = user.articles.build({
+        title: Faker::Lorem.characters(number: 10),
+        content: Faker::Lorem.characters(number: 30)
+      })
+    end
 
-
+    it "記事を保存できる" do
+      expect(@article).to be_valid
+    end
+  end
 end
