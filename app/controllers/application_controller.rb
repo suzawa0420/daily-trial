4d19ae9b9b06bb88f60574e649cce88308496668
suzawa0420 <suzawa@photomo.jp>
2022-07-12 16:08:27 +0900

    英語表示の切り替え

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
 
   def current_user
     ActiveDecorator::Decorator.instance.decorate(super) if super.present?
     super
+  end
+
+  # これは自動で実行される
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  private
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 
 end
