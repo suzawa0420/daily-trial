c6f90245398d94b573b5c2994eef32fb04658238
suzawa0420 <suzawa@photomo.jp>
2022-07-13 11:11:14 +0900

    保存失敗のテスト

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
+  let!(:user) do
+    User.create!({
+    email: "test@example.com",
+    password: "passord"
+    })
+  end
   context "タイトルと内容が入力されている場合" do
-    let!(:user) do
-      User.create!({
-      email: "test@example.com",
-      password: "passord"
-      })
-    end
 
     let!(:article) do
       user.articles.build({
@@ -31,4 +31,18 @@ RSpec.describe Article, type: :model do
       expect(article).to be_valid
     end
   end
+
+  context "タイトルの文字が一文字の場合" do
+    let!(:article) do
+      user.articles.create({
+        title: Faker::Lorem.characters(number: 1),
+        content: Faker::Lorem.characters(number: 30)
+      })
+    end
+
+    it "記事を保存できない" do
+      expect(article.errors.messages[:title][0]).to eq("は2文字以上で入力してください")
+    end
+  end
+
 end
